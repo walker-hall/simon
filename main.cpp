412ecc38da02650848c691733e01a2f5b009736d
frostyio <57603739+frostyio@users.noreply.github.com>
2024-03-14 17:36:55 -0600

    updated docs

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 #include <QApplication>
 
 ///
-/// \brief main
-/// \param argc
-/// \param argv
-/// \return
-///
 /// Main entry point for the SimonGame
+///
+/// github usernames: @frostyio , @walker-hall
+///
+/// https://github.com/UofU-CS3505/cs3505-assignment6-frostyio
+///
+/// Our special feature was adding a redemption screen so that when you die,
+/// you have a chance to redeem yourself by completeing a pattern that gets significantly more
+/// difficult each time you die.
 ///
 /// By Walker Hall and Zak Holt
 /// March 14, 2024
+///
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
